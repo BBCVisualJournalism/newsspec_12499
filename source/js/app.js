75f281255a8c35487de664507217889aff4fdbf4
Punit Shah <punit.shah@bbc.co.uk>
2015-11-23 17:54:30 +0000

    implemented png fallback for svgs
    checks if svg is supported and swaps the svg img sources with the corresponding pngs if it isn't supported

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,7 @@
-define(['lib/news_special/bootstrap'], function (news) {
+define(['lib/news_special/bootstrap', 'svg_as_img'], function (news, SvgAsImg) {
     $.emit('init_images');
+
+    SvgAsImg.replaceSvgs();
+
     news.sendMessageToremoveLoadingImage();
 });
