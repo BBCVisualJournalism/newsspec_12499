826ffe0b3e629fe1e702cdf823805fc98f4f09b4
Punit Shah <punit.shah@bbc.co.uk>
2015-11-26 17:41:02 +0000

    using real vpids
    current video for siberia is the same as the video for syria - needs updating

--- source/js/app.js
+++ source/js/app.js
@@ -3,12 +3,12 @@ define(['lib/news_special/bootstrap', 'video', 'bind.polyfill'], function (news,
 
     $.emit('init_images');
 
-    new Video('#section_video_container-venezuela', 'p02w03kx', 'http://news.stage.bbc.co.uk/news/special/2015/newsspec_12499/content/english/img/venezuela/venezuela_poster_image_1290.jpg');
-    new Video('#section_video_container-syria', 'p02w03kx', 'http://news.stage.bbc.co.uk/news/special/2015/newsspec_12499/content/english/img/syria/syria_poster_image_1290.jpg');
-    new Video('#section_video_container-siberia', 'p02w03kx', 'http://news.stage.bbc.co.uk/news/special/2015/newsspec_12499/content/english/img/siberia/siberia_poster_image_1290.jpg');
-    new Video('#section_video_container-pakistan', 'p02w03kx', 'http://news.stage.bbc.co.uk/news/special/2015/newsspec_12499/content/english/img/pakistan/pakistan_poster_image_1290.jpg');
-    new Video('#section_video_container-us', 'p02w03kx', 'http://news.stage.bbc.co.uk/news/special/2015/newsspec_12499/content/english/img/us/us_poster_image_1290.jpg');
-    new Video('#section_video_container-congo', 'p02w03kx', 'http://news.stage.bbc.co.uk/news/special/2015/newsspec_12499/content/english/img/congo/congo_poster_image_1290.jpg');
+    new Video('#section_video_container-venezuela', 'p03912vm', 'http://news.stage.bbc.co.uk/news/special/2015/newsspec_12499/content/english/img/venezuela/venezuela_poster_image_1290.jpg');
+    new Video('#section_video_container-syria', 'p0393rt0', 'http://news.stage.bbc.co.uk/news/special/2015/newsspec_12499/content/english/img/syria/syria_poster_image_1290.jpg');
+    new Video('#section_video_container-siberia', 'p0393rt0', 'http://news.stage.bbc.co.uk/news/special/2015/newsspec_12499/content/english/img/siberia/siberia_poster_image_1290.jpg');
+    new Video('#section_video_container-pakistan', 'p0393v4l', 'http://news.stage.bbc.co.uk/news/special/2015/newsspec_12499/content/english/img/pakistan/pakistan_poster_image_1290.jpg');
+    new Video('#section_video_container-us', 'p0394ktk', 'http://news.stage.bbc.co.uk/news/special/2015/newsspec_12499/content/english/img/us/us_poster_image_1290.jpg');
+    new Video('#section_video_container-congo', 'p03941tm', 'http://news.stage.bbc.co.uk/news/special/2015/newsspec_12499/content/english/img/congo/congo_poster_image_1290.jpg');
 
     news.sendMessageToremoveLoadingImage();
 });
