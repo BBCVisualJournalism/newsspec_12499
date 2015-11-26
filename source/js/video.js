5d0bda05ae234a719e8a0c43ea3b14863f114396
Punit Shah <punit.shah@bbc.co.uk>
2015-11-26 14:18:41 +0000

    fixed iOS video bug
    when playing a video on iOS, the video is played in its own window. when the user taps 'Done' to stop the video and go back to the webpage, the play button doesn't reappear.
    fix: enabling the smp cta and hiding the custom overlay on mobile devices, and disabling the smp cta and showing the custom overlay on devices wider than 748px
    using '-hidden' class on overlay to hide it instead of using jquery's .hide()/.show()
    rebuilt custom jquery with dimensions module included so i can use .width() when handling window resize

--- source/js/video.js
+++ source/js/video.js
@@ -1,13 +1,14 @@
-define(['lib/news_special/bootstrap', 'bump-3'], function (news, $) {
+define(['lib/news_special/bootstrap', 'bump-3'], function (news, bump) {
 
     var Video = function (videoContainerSelector, vpid, holdingImage) {
         this.selector = videoContainerSelector;
         this.$videoContainer = news.$(this.selector);
-        this.$video = $(this.selector).find('.section_video');
+        this.videoEl = bump(this.selector).find('.section_video');
         this.vpid = vpid;
         this.holdingImage = holdingImage;
         this.mp = null;
         this.$overlay = this.$videoContainer.find('.section_video_overlay');
+        this.cta_breakpoint = 612;
 
         this.init();
     };
@@ -25,30 +26,65 @@ define(['lib/news_special/bootstrap', 'bump-3'], function (news, $) {
                     holdingImageURL: this.holdingImage
                 },
                 responsive: true,
-                autoplay: false,
-                ui: {
-                    cta: {
-                        enabled: false
-                    }
-                }
+                autoplay: false
             };
-            this.mp = this.$video.player(playerSettings);
-            this.mp.bind('ended', this.videoEnded.bind(this));
+            this.mp = this.videoEl.player(playerSettings);
             this.mp.load();
+            this.setEvents();
+            
+            if (this.getVideoWidth() >= this.cta_breakpoint) {
+                this.disableSmpCta();
+            }
+        },
+
+        setEvents: function () {
+            this.mp.bind('ended', this.videoEnded.bind(this));
+            this.mp.bind('playing', this.hideOverlay.bind(this));
+
+            var self = this;
+            news.$(window).on('resize', function () {
+                if (self.getVideoWidth() >= self.cta_breakpoint) {
+                    self.disableSmpCta();
+                } else {
+                    self.enableSmpCta();
+                }
+            });
         },
 
         playVideo: function () {
-            this.$overlay.hide();
+            this.hideOverlay();
             this.mp.play();
         },
 
+        hideOverlay: function () {
+            this.$overlay.addClass('section_video_overlay-hidden');
+        },
+
         videoEnded: function () {
-            this.$overlay.show();
+            this.$overlay.removeClass('section_video_overlay-hidden');
         },
 
         videoStop: function () {
             this.videoEnded();
             this.mp.stop();
+        },
+
+        getVideoWidth: function () {
+            return this.$videoContainer.width();
+        },
+
+        disableSmpCta: function () {
+            var ui_config = {
+                cta: { enabled: false }
+            };
+            this.mp.updateUiConfig(ui_config);
+        },
+
+        enableSmpCta: function () {
+            var ui_config = {
+                cta: { enabled: true }
+            };
+            this.mp.updateUiConfig(ui_config);
         }
     };
 
